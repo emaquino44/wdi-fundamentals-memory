b98b0498527236230a52f675e3f99f345cf8ee46
emaquino44 <prexy44@yahoo.com>
2017-03-23 23:39:36 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,45 @@
-console.log("Up and running!");
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-var cards = ['queen', 'queen', 'king', 'king'];
+console.log("User flipped " + "queen");
+console.log("User flipped " + "king");
+var cards = [
+	{	
+		rack: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png',
+	},
+	{
+		rack: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png',
+	},
+	{
+		rack: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rack: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
+];
 var cardsInPlay = [];
-cardsInPlay.push('cardOne', 'cardTwo');
+cardsInPlay.push('queen', 'king');
 var cardOne = cardsInPlay[0];
 var cardTwo = cardsInPlay[2];
 console.log("User flipped king" + cardTwo);
 if(cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[2]) {
-		alert("You've found a match")
+		alert("You've found a match") 
 	} else {
 		alert("Sorry, try again.");
 	}
 }
-
-
-
-
+var flipCard = function() {
+	console.log("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
+}
 
 
 
