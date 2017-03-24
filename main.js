070e1e52b4a7248d70cdb433394569f77227c864
emaquino44 <prexy44@yahoo.com>
2017-03-23 18:25:51 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour= "king";
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+cardsInPlay.push('cardOne', 'cardTwo');
+var cardOne = cardsInPlay[0];
+var cardTwo = cardsInPlay[2];
+console.log("User flipped king" + cardTwo);
+if(cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[2]) {
+		alert("You've found a match")
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+
+
+
+
+
+
